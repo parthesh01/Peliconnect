b88cbef5dc21a27d60c7b45e045115e959ee1cc7
unknown <parthesh01@gmail.com>
2024-11-26 16:38:47 +0530

    ain

--- src/_root/pages/Explore.tsx
+++ src/_root/pages/Explore.tsx
@@ -31,8 +31,8 @@ const Explore = () => {
     )
   }
 
-  const shouldShowSearchResults = searchValue !== '';
-  const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((item) => item.documents.length === 0)
+  const shouldShowSearchResults = searchValue !== "";
+  const shouldShowPosts = !shouldShowSearchResults && posts.pages.every((item:) => item.documents.length === 0)
 
   return (
     <div className='explore-container'>
@@ -73,7 +73,7 @@ const Explore = () => {
           /> 
         ) : shouldShowPosts ? (
           <p className='w-full mt-10 text-center text-light-4'>End of posts</p>
-        ) : posts.pages.map((item, index) => (
+        ) : posts.pages.map((item:, index) => (
           <GridPostList key={`page-${index}`} posts={item.documents} />
         ))}
       </div>
--- src/components/shared/SearchResults.tsx
+++ src/components/shared/SearchResults.tsx
@@ -2,22 +2,22 @@ import { Models } from 'appwrite';
 import Loader from './Loader';
 import GridPostList from './GridPostList';
 
-type SearchResultProps = {
+export type SearchResultProps = {
     isSearchFetching: boolean;
-    searchedPosts: Models.Document
+    searchedPosts: any
 }
 
 const SearchResults = ({ isSearchFetching, searchedPosts}: SearchResultProps) => {
-    if(isSearchFetching) return <Loader />
+    if(isSearchFetching) {return <Loader />}
 
-    if(searchedPosts && searchedPosts.documents.length > 0) {
+    else if(searchedPosts && searchedPosts.documents.length > 0) {
         return (
             <GridPostList posts={searchedPosts.documents}/>
         )
-    } 
+    } else{
   return (
     <p className='w-full mt-10 text-center text-light-4'>No results found</p>
-  )
+  )}
 }
 
 export default SearchResults
